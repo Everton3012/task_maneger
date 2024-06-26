2aa0a0c78ac70aabe91361c2bf20997a122ed3e0
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-06-25 21:21:18 -0300

    analitcs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.scss";
 import { Header } from "@/components/Header";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={inter.className}>
-        <Header/>{children}</body>
+          <Header />
+            {children}
+          <Analytics />
+        </body>
     </html>
   );
 }
